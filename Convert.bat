320f63715cfb70bcf1579ae7149889d566832db7
xtended77 <109032914+xtended77@users.noreply.github.com>
2024-11-15 21:35:23 +0500

    Add files via upload

---
+++ Convert.bat
@@ -0,0 +1,30 @@
+@set startTime=%time%
+@ echo off
+: List of codepages https://ss64.com/nt/chcp.html
+CHCP 65001
+set output_dir=E:\Video\The Fall (Падение) S01_03
+
+echo -----------------------------
+set avidemux="C:\Program Files\Avidemux\avidemux.exe"
+
+if not exist "%output_dir%" (
+  mkdir "%output_dir%"
+  echo Output directory not exist...
+  echo "%output_dir%" created.
+)
+
+echo -----------------------------
+echo Start encoding at %time%
+echo Output dir "%output_dir%"
+echo -----------------------------
+for %%f in (*.avi *.mkv *.mov *.mp4 *.mts) do (
+  echo Processing %%~ff
+  %avidemux% --load "%%f" --run Settings.py --save "%output_dir%\%%~nf.mp4" --quit
+)
+
+echo -----------------------------
+echo Start  time: %startTime%
+echo Finish time: %time%
+echo -----------------------------
+
+Pause
